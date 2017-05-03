e8ee9f22172fce425afde0fbbf42eb1d3e0334d5
leon3110l <leon3110l@gmail.com>
2017-05-03 11:16:51 +0200

    added groen stuff

---
+++ src/groenTemplate.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>groen</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">navItem</a>
+            <ul>
+              <li><a href="#">subMenuItem</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">navItem</a>
+            <ul>
+              <li><a href="#">subMenuItem</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
